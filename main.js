f124118820554abc4b40bb98cd743dbfa815a3f7
Nidhishree N <161071744+nidhi-shree@users.noreply.github.com>
2024-10-27 19:40:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+
+particlesJS("particles-js",{
+    "particles": {
+        "number": {
+            "value": 100,
+            "density": { "enable": true, "value_area": 800 }
+        },
+        "color": { "value": "#ffffff"},
+        "shape": { "type": "circle" },
+        "opacity": {
+            "value": 0.8,
+            "random": true,
+            "anim": { "enable": true, "speed": 2, "opacity_min": 0.3 }
+        },
+        "size": {
+            "value": 5,
+            "random": true,
+            "anim": { "enable": true, "speed": 5, "size_min": 0.5 }
+        },
+        "line_linked": {
+            "enable": true,
+
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1.5
+        },
+        "move": {
+            "enable": true,
+            "speed": 0.8,
+            "direction": "none",
+            "random": true,
+            "out_mode": "out"
+
+        },
+        "anim": { "enable": true, "speed": 1, "opacity_min": 0.1 }
+    },
+
+
+});
